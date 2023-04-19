e7c759700aec34e5fc244d3f85c5078b29ef2158
Eya <105926595+eyaznt@users.noreply.github.com>
2023-04-19 16:39:20 +0100

    Delete IOT_Pet_Planter/src/main/java/com/Whitecape/PFE/exceptions directory

--- IOT_Pet_Planter/src/main/java/com/Whitecape/PFE/exceptions/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.Whitecape.PFE.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
